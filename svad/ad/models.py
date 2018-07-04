77a9401d710060eae9ecab31402d71225f553073
Mohammed Shalabi <idevmedo@gmail.com>
2018-07-05 01:06:52 +0300

    I've changed models.py and added all our tables (classes) and their attributes

--- svad/ad/models.py
+++ svad/ad/models.py
@@ -9,21 +9,41 @@ class Country(models.Model):
     name = models.CharField(max_length=30)
 
 class City(models.Model):
-    name = models.CharField(max_length=20)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    
+    name = models.CharField(max_length=20)
+    
 
 class Airport(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
+
     name = models.CharField(max_length=30)
+    is_civilian = models.BooleanField()
+    is_military= models.BooleanField()
+    is_international= models.BooleanField()
 
 # Primary keys and Foreign keys and relationships 
 # #one to many and one to one
 #
 #
 
+class Aircraft_Type(models.Model):
+    type = models.CharField(max_length=30)
+    max_fuel_capacity = models.IntegerField()
+    seats = models.IntegerField(max_length=6)
+
 class Aircraft(models.Model):
-    pass
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    aircraft_type = models.ForeignKey(Aircraft_Type,on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    production_year = models.DateField()
+    status = models.CharField(max_length=30)
+    fuel_level = models.FloatField()
+    fuel_currency = models.CharField(max_length=3)
+
 
 class AirportAircraftTypeAuth(models.Model):
-    pass
-
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    aircraft_type = models.ForeignKey(Aircraft_Type,on_delete=models.CASCADE)
