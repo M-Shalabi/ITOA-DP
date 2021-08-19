5d4fda680c7b03d55538a36c37d73b51d4f1dff1
Mohammed Shalabi <mohammedshalabi@Mohammeds-MacBook-Pro.local>
2021-08-19 10:31:37 +0300

    I added documentation

--- svad/config/settings.py
+++ svad/config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ad.db',
         'USER': 'postgres',
-        'PASSWORD': 'Faisal',
+        'PASSWORD': 'idevmedo@gmail.com',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
